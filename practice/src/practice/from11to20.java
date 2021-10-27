59e905ad30cbcae9161792825a52cb8d021b4260
G3neration <90040877+G3neration@users.noreply.github.com>
2021-10-27 17:30:43 +0300

    To pull

--- practice/src/practice/from11to20.java
+++ practice/src/practice/from11to20.java
@@ -8,7 +8,7 @@ public class from11to20 {
 				repeatingSymbols+=word.charAt(i);
 		return repeatingSymbols;
 	}
-public static int differenceMaxMin (int[] arrayForEx12) {
+	public static int differenceMaxMin (int[] arrayForEx12) {
 	int[] MinMax = {arrayForEx12[0], arrayForEx12[0]};
 	for (int i = 0; i < arrayForEx12.length; i++)
 	{ 	
@@ -20,11 +20,28 @@ public static int differenceMaxMin (int[] arrayForEx12) {
 	int dif = MinMax[1] - MinMax[0];
 	return dif;
 }
-	
+	public static boolean isAvgWhole (int[] arrayForEx13) {
+	double sum = 0;
+	for (int i = 0; i < arrayForEx13.length; i++) {
+		sum += arrayForEx13[i];
+}
+
+	double rez = sum % arrayForEx13.length;
+	return rez == 0;
+}
+	//public static int[] cumulativeSum (int[] arrayForEx14, int [] resultForEx14) {
+	//for (int i = 1; i < arrayForEx14.length; i++)
+	//resultForEx14[i] += resultForEx14[i-1];
+	//return resultForEx14;
+	//}
 	public static void main(String[] args) {
 		System.out.println(repeat("mice", 5));
 		int[] arrayForEx12 = {10, 4, 1, 4, -10, -50, 32, 21};
 		System.out.println(differenceMaxMin(arrayForEx12));
-
+		int[] arrayForEx13 = {1, 2, 3, 4};
+		System.out.println(isAvgWhole(arrayForEx13));
+		int [] arrayForEx14 = {3, 3, -2, 408, 3, 3};
+		int [] resultForEx14 = new int [6];
+		System.out.println(cumulativeSum(arrayForEx14, resultForEx14));
 	}
 }
